f8e9ee4b1770612f8e0ea54f0ab81e202a95c3b8
Mauricio Aniche <mauricioaniche@gmail.com>
2010-07-10 01:22:48 -0300

    initial spike of the report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,5 @@
 class ReportsController < ApplicationController
   def create
     @tags = Tag.report_by_percentage params[:initial_date], params[:end_date], params[:percentage]
-    
-    
   end
 end
