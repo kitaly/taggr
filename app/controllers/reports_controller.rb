8488055143fcaed892ae795fd995e4a5761f21dd
Mauricio Aniche <mauricioaniche@gmail.com>
2010-07-10 20:38:32 -0300

    setting percentage in report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
   def report
     @tags = Tag.report_by_percentage params[:initial_date], params[:end_date], params[:percentage]
+    @total = @tags.inject(0) { |sum, e| sum + e.total }
   end
   
   def create
